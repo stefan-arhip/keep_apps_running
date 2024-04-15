b068ac1bf05a3758e01681ddb9f5422f31b837ee
Ștefan Arhip <stefan.arhip@gmail.com>
2024-04-15 21:57:21 +0300

    comment line in filelist

--- Keep_Apps_Running.ps1
+++ Keep_Apps_Running.ps1
@@ -2,36 +2,38 @@
 
 while ($True) {
     Get-Content '.\filelist.txt' | ForEach-Object {
-        $var = $_.Split('=')
+        $var = $_.Split('=') 
 
         New-Variable -Name AppName -Value $var[0]
         New-Variable -Name AppPath -Value $var[1]
         New-Variable -Name AppParam -Value $var[2]
 
-    
-        $AppIsRunning = 0
-	    $CurrentTime = Get-Date -Format "yyyymmdd-HHmmss"
-        (Get-Process).ProcessName | ForEach-Object {
-            if ($_ -imatch $AppName) {
-                $AppIsRunning++            
-                Write-Host $currentTime '-'$_ 'is running'
-            }
+        if ($AppName.Substring(0, [Math]::Min($AppName[0].Length, 20)) -Match '#') {
+            # comment line in file
         }
-        
-        if ($AppIsRunning -eq 0){
-            Write-Host $CurrentTime '- launch'$AppName
-            if ($AppParam -eq "") {
-                Start-Process -FilePath $AppPath
-            } 
-            else {
-                Start-Process -FilePath $AppPath $AppParam
+        else {
+            $AppIsRunning = 0
+	        $CurrentTime = Get-Date -Format "yyyymmdd-HHmmss"
+            (Get-Process).ProcessName | ForEach-Object {
+                if ($_ -imatch $AppName) {
+                    $AppIsRunning++            
+                    Write-Host $currentTime '-'$_ 'is running'
+                }
             }
+            if ($AppIsRunning -eq 0){
+                Write-Host $CurrentTime '- launch'$AppName
+                if ($AppParam -eq "") {
+                    Start-Process -FilePath $AppPath
+                } 
+                else {
+                    Start-Process -FilePath $AppPath $AppParam
+                }
+            }            
         }
 
         Remove-Variable -Name AppName
         Remove-Variable -Name AppPath
         Remove-Variable -Name AppParam 
-
-        } 
+    } 
     Start-Sleep -Seconds $Interval
 }
